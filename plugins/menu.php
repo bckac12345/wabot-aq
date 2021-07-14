6eb3a3083fb2431ed36401b8b28c1f4dd843e277
bckac12345 <86071428+bckac12345@users.noreply.github.com>
2021-07-14 22:51:42 +0700

    Update and rename menu.js to menu.php

---
+++ plugins/menu.php
@@ -0,0 +1,38 @@
+<?php
+//Get the status and decode the JSON
+$status = json_decode(file_get_contents('https://api.mcsrvstat.us/2/domain.tld'));
+
+//Show the version
+echo $status->version;
+
+//Show a list of players
+foreach ($status->players->list as $player) {
+	echo $player.'<br />';
+}
+handler.help = ['menu', 'help', '?']
+handler.tags = ['main']
+handler.command = /^(menu|help|\?)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 3
+
+module.exports = handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
+?>
